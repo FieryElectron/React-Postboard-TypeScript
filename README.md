e2d15603fb935319935e30cfdbad850a37d3ee7f
FieryElectron <chenyiyanggermany@gmail.com>
2022-04-22 20:22:59 +0200

    cleaned template

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # React-Postboard-TypeScript
 
 This project is converted from (https://github.com/FieryElectron/React-Postboard).
+
+## Notes
+
+To enable **createRoot** with **TypeScript**, **@types/react-dom@^18.0.0** need to be intalled.
+
+    npm i @types/react-dom@^18.0.0
+
+Add **!** when the value is never **null**.
+
+Before
+
+    const root = createRoot(document.getElementById('root'));
+
+After
+
+    const root = createRoot(document.getElementById('root')!);
---
+++ react-postboard-typescript/src/App.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+let container: HTMLElement;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container.remove();
+});
+
+test('renders without crashing', () => {
+  createRoot(container).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+});
